7e643f532bc167b34064504e41f12ac198b11252
pdineshyadav <p.dineshyadav@yahoo.in>
2018-10-30 18:48:08 -0400

    Environment Page UI

--- cmautomation/src/main/java/com/cmautomation/spring/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.cmautomation.spring.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
